0687d02083e78e8d97a53dda324652ac33154597
Lawrence N. Masullo <lmasullo@gmail.com>
2021-04-17 10:59:24 -0500

    back to onmount and dep

--- src/lib/GraphQL.js
+++ src/lib/GraphQL.js
@@ -1,58 +1,58 @@
-import { GraphQLClient } from "graphql-request";
+//import { GraphQLClient } from "graphql-request";
 //import * as GraphQLClient from "graphql-request";
 
-let evalTemplates
+// let evalTemplates
 
-const endpoint = "https://api.8base.com/ckb9f8fky000207lb1vuoc6kv";
+// const endpoint = "https://api.8base.com/ckb9f8fky000207lb1vuoc6kv";
 
-    const MY_TOKEN = "6d8d2cba-7ee7-4b33-b84d-8a7ea2e4691b";
+//     const MY_TOKEN = "6d8d2cba-7ee7-4b33-b84d-8a7ea2e4691b";
 
-    const graphQLClient = new GraphQLClient(endpoint, {
-      headers: {
-        authorization: `Bearer ${MY_TOKEN}`,
-      },
-    });
+//     const graphQLClient = new GraphQLClient(endpoint, {
+//       headers: {
+//         authorization: `Bearer ${MY_TOKEN}`,
+//       },
+//     });
 
-    const query = `
-      {
-        edshiftsresEvalTemplatesList(filter: { deleted: { equals: false } }) {
-          items {
-            id
-            templateName
-          }
-        }
-      }
-    `;
+//     const query = `
+//       {
+//         edshiftsresEvalTemplatesList(filter: { deleted: { equals: false } }) {
+//           items {
+//             id
+//             templateName
+//           }
+//         }
+//       }
+//     `;
 
 
-    function resolveAfter2Seconds() {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve('resolved');
-        }, 2000);
-      });
-    }
+//     function resolveAfter2Seconds() {
+//       return new Promise(resolve => {
+//         setTimeout(() => {
+//           resolve('resolved');
+//         }, 2000);
+//       });
+//     }
     
-    async function asyncCall() {
-      console.log('calling');
-      //const result = await resolveAfter2Seconds();
+//     async function asyncCall() {
+//       console.log('calling');
+//       //const result = await resolveAfter2Seconds();
       
-      // expected output: "resolved"
-      const data = await graphQLClient.request(query);
- 		  evalTemplates = data.edshiftsresEvalTemplatesList.items;
- 		  console.log('evalTemplates: ', evalTemplates);
-      //console.log(result);
-    }
+//       // expected output: "resolved"
+//       const data = await graphQLClient.request(query);
+//  		  evalTemplates = data.edshiftsresEvalTemplatesList.items;
+//  		  console.log('evalTemplates: ', evalTemplates);
+//       //console.log(result);
+//     }
     
-    asyncCall();
+//     asyncCall();
     
 
-// async asyncData() {
-//     
+// // async asyncData() {
+// //     
 
-// }
+// // }
 
 
-//const value=123
-export default evalTemplates
+// //const value=123
+// export default evalTemplates
    
